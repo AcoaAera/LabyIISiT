2bac295841ca7e10e809ee4af1da2a15227a62bc
AcoaAera <korablewaleksei1997@yandex.ru>
2018-12-04 22:49:07 +0500

    Lab3

---
+++ lab3 [сем. сети]/lab3/FormNODE/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace FormNODE
+{
+    public partial class Form1 : Form
+    {
+        //Беляев Иван - IvanBelyaev13
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
---
+++ lab3 [сем. сети]/lab3/ModuleTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FormNODE;
+using System.Collections.Generic;
+
+namespace ModuleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Рустем Хатямов
+        }
+    }
+}
