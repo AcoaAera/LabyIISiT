4978568447ee8b2379bed65fc5382e9b21d1db2d
AcoaAera <korablewaleksei1997@yandex.ru>
2018-12-14 14:21:48 +0500

    create project

---
+++ lab5 [НС]/NeuralTrainings/NeuralTrainings/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace NeuralTrainings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void RecognitionBtn_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+        }
+
+        private void PbClearBtn_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void StudyModeRB_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void RecognitionModeRB_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
---
+++ lab5 [НС]/NeuralTrainings/NeuralTrainings/Neuron.cs
@@ -0,0 +1,6 @@
+﻿namespace NeuralTrainings
+{
+    class Neuron
+    {
+    }
+}
