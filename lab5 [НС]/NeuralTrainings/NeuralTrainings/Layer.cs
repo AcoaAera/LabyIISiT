1c7a57bf68ca52f8af47d3d476a4966a74eece54
laceratione <laceratione@gmail.com>
2018-12-14 18:20:57 +0500

    class Lauer

--- lab5 [НС]/NeuralTrainings/NeuralTrainings/Layer.cs
+++ lab5 [НС]/NeuralTrainings/NeuralTrainings/Layer.cs
@@ -8,6 +8,86 @@ namespace NeuralTrainings
 {
     class Layer
     {
-        
+        private List<Link> layer;
+        private int inputNeuronsCount, outputNeuronsCount;
+        private const double errorFixValue = 0.15;
+
+        public Layer(int inputNeuronsCount, int outputNeuronsCount, double[,] weights)
+        {
+            this.inputNeuronsCount = inputNeuronsCount;
+            this.outputNeuronsCount = outputNeuronsCount;
+            layer = new List<Link>();
+
+            for (int i = 0; i < inputNeuronsCount; i++)
+            {
+                Neuron firstLayerNeuron = new Neuron();
+
+                for (int j = 0; j < outputNeuronsCount; j++)
+                    layer.Add(new Link(firstLayerNeuron, new Neuron()));  //layer.Add(new Link(new Neuron(), new Neuron()));
+            }
+        }
+
+        public bool GetNeuronSignal(int linkIndex)
+        {
+            return layer[linkIndex].Neuron1.Signal;
+        }
+
+        public void SetNeuronSignal(int linkIndex, bool signalValue)
+        {
+            layer[linkIndex].Neuron1.Signal = signalValue;
+        }
+
+        public double GetWeight(int linkIndex)
+        {
+            return layer[linkIndex].WeightValue;
+        }
+
+        public void SetWeight(int linkIndex, double weightValue)
+        {
+            layer[linkIndex].WeightValue = weightValue;
+        }
+
+        public void WeightsFix(int neuronSignal, int neuronIndex)
+        {
+            for (int i = neuronIndex; i < layer.Count; i += outputNeuronsCount)
+            {
+                if (layer[i].Neuron1.Signal) // активный нейрон
+                {
+                    if (neuronSignal == 0)
+                        layer[i].WeightValue += errorFixValue;
+                    else
+                        layer[i].WeightValue -= errorFixValue;
+                }
+            }
+        }
+
+        public double[,] GetWeights()
+        {
+            double[,] weights = new double[inputNeuronsCount, outputNeuronsCount];
+
+            int i = 0, j = 0;
+
+            for (int k = 0; k < layer.Count; k++)
+            {
+                weights[i, j++] = layer[k].WeightValue;
+
+                if ((k + 1) % outputNeuronsCount == 0)
+                {
+                    i++;
+                    j = 0;
+                }
+            }
+
+            return weights;
+        }
+
+        public void SignalsReset()
+        {
+            foreach (var link in layer)
+            {
+                link.Neuron1.Signal = false;
+                link.Neuron2.Signal = false;
+            }
+        }
     }
 }
